cf5dfed22e0cfa59b514d27b7fbfdd338a8072a5
Piotreqsl <piotreksl2002@go2.pl>
2023-04-01 15:32:52 +0200

    Bottom panel wokings

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { store, persistor } from "./src/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -9,12 +8,12 @@ import Index from "./src/index";
 // Import React
 import React from "react";
 // Import required components
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 // Import Map and Marker
 import MapView, { Marker } from "react-native-maps";
 import LocationMap from "./src/Map";
 import BottomPanel from "./src/BottomPanel";
-
+import { registerRootComponent } from "expo";
 const App = () => {
   return (
     <Provider store={store}>
--- src/BottomPanel.js
+++ src/BottomPanel.js
@@ -1,24 +1,23 @@
-
-import React, { useCallback, useMemo, useRef } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useCallback, useMemo, useRef } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 import {
   BottomSheetModal,
   BottomSheetModalProvider,
-} from '@gorhom/bottom-sheet';
+} from "@gorhom/bottom-sheet";
 
 const BottomPanel = () => {
   // ref
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const bottomSheetModalRef = useRef(null);
 
   // variables
-  const snapPoints = useMemo(() => ['25%', '50%'], []);
+  const snapPoints = useMemo(() => ["25%", "50%"], []);
 
   // callbacks
   const handlePresentModalPress = useCallback(() => {
     bottomSheetModalRef.current?.present();
   }, []);
   const handleSheetChanges = useCallback((index) => {
-    console.log('handleSheetChanges', index);
+    console.log("handleSheetChanges", index);
   }, []);
 
   // renders
@@ -49,13 +48,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    justifyContent: 'center',
-    backgroundColor: 'grey',
+    justifyContent: "center",
+    backgroundColor: "grey",
   },
   contentContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
 });
 
-export default BottomPanel
+export default BottomPanel;
