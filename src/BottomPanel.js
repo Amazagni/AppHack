daa90819ab15f794f81416912e22af6b0bba5326
Piotreqsl <piotreksl2002@go2.pl>
2023-04-01 19:15:27 +0200

    Fixed gesture height

--- src/BottomPanel.js
+++ src/BottomPanel.js
@@ -1,53 +1,67 @@
 import React, { useCallback, useMemo, useRef, useEffect } from "react";
-import { View, Text, StyleSheet, Button, Image, ScrollView } from "react-native";
 import {
-  BottomSheetModal,
-  BottomSheetModalProvider,
-} from "@gorhom/bottom-sheet";
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  ScrollView,
+} from "react-native";
+import { BottomSheetView } from "@gorhom/bottom-sheet";
 import { greaterOrEq } from "react-native-reanimated";
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
 import LocationMap from "./Map";
 
 const BottomPanel = () => {
   // ref
-  const bottomSheetModalRef = useRef(null);
-  
+  const sheetRef = useRef(null);
+
   // variables
-  const snapPoints = useMemo(() => ["25%", "50%"], []);
+  const snapPoints = useMemo(() => ["25%", "50%", "90%"], []);
 
-  useEffect(() => {
-    bottomSheetModalRef.current?.present();
-  },[])
-
-  const handleSheetChanges = useCallback((index) => {
-    console.log("handleSheetChanges", index);
+  const handleSheetChange = useCallback((index) => {
+    console.log("handleSheetChange", index);
   }, []);
+  const handleSnapPress = useCallback((index) => {
+    sheetRef.current?.snapToIndex(index);
+  }, []);
+  const handleClosePress = useCallback(() => {
+    sheetRef.current?.close();
+  }, []);
+
+  const renderItem = useCallback(
+    ({ item }) => (
+      <View style={styles.itemContainer}>
+        <Text>{item}</Text>
+      </View>
+    ),
+    []
+  );
 
   // renders
   return (
-    <BottomSheetModalProvider>
-      <View style={styles.container}>
-        <LocationMap/>
-        <View style={styles.contentBackground}/>
-        <BottomSheetModal
-          ref={bottomSheetModalRef}
-          index={1}
-          snapPoints={snapPoints}
-          onChange={handleSheetChanges}
-        >
-          <View style={styles.contentContainer}>
-            <Text style={styles.titleText}>Nickname</Text>
-            <Image style={styles.icon} source={require('../assets/icon.png')}></Image>
-            <Text>Map completion: 70%</Text>
-            <View style={styles.expBar}>
-              <View style={styles.expProgress}></View>
-            </View>
-            <Text style={styles.levelText}>Level: 20</Text>
-            <Button title="Achievements" color={"green"}>
-            </Button>
+    <View style={styles.container}>
+      <LocationMap />
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        onChange={handleSheetChange}
+      >
+        <View style={styles.contentContainer}>
+          <Text style={styles.titleText}>Nickname</Text>
+          <Image
+            style={styles.icon}
+            source={require("../assets/icon.png")}
+          ></Image>
+          <Text>Map completion: 70%</Text>
+          <View style={styles.expBar}>
+            <View style={styles.expProgress}></View>
           </View>
-        </BottomSheetModal>
-      </View>
-    </BottomSheetModalProvider>
+          <Text style={styles.levelText}>Level: 20</Text>
+          <Button title="Achievements" color={"green"}></Button>
+        </View>
+      </BottomSheet>
+    </View>
   );
 };
 
@@ -55,43 +69,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    borderRadius: 50
+    borderRadius: 50,
   },
   contentContainer: {
     flex: 1,
     alignItems: "center",
-    
   },
   expBar: {
-    width: '70%',
+    width: "70%",
     height: 15,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 2,
-    marginBottom: 20
+    marginBottom: 20,
   },
   expProgress: {
-    width: '70%',
-    height: '100%',
-    backgroundColor: 'orange'
+    width: "70%",
+    height: "100%",
+    backgroundColor: "orange",
   },
   icon: {
     height: 50,
     width: 50,
-    marginBottom: 15
-
+    marginBottom: 15,
   },
   titleText: {
     fontSize: 25,
-    fontWeight: 'bold',
-    marginBottom: 10
+    fontWeight: "bold",
+    marginBottom: 10,
   },
   levelText: {
     fontSize: 25,
-    marginBottom: 25
+    marginBottom: 25,
   },
   contentBackground: {
-    height: '45%'
-  }
+    height: "45%",
+  },
 });
 
 export default BottomPanel;
