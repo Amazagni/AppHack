085dc6f3fde78741f115eccc318e3882426d1d48
Wojciech Łoboda <loboda.w28@gmail.com>
2023-04-01 23:12:05 +0200

    quest

--- src/ActiveQuest.js
+++ src/ActiveQuest.js
@@ -3,7 +3,7 @@
 // Import React
 import React, { useEffect } from "react";
 // Import required components
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet, View, Image, Text} from "react-native";
 import { useDispatch, connect, useSelector } from "react-redux";
 import { fetchPoints, selectPoints } from "./store/slices/pointsSlice";
 // Import Map and Marker
@@ -14,18 +14,35 @@ const ActiveQuest = () => {
   //  dispatch(fetchPoints());
   //}, []);
 
-  return <View style={styles.box}></View>;
+  return (<View style={styles.box}>
+    <Image style={styles.icon}source={require('../assets/excl.png')}></Image>
+    <Text style={{marginTop: 12, marginLeft: 12, marginRight: 25}}>HGKJHKJHGKJG</Text>
+  </View>);
 };
 export default ActiveQuest;
 
+//source={
+//    discovered
+//      ? require("../assets/yellowexcl.png")
+//      : require("../assets/quest.png")
+//  }
+
 const styles = StyleSheet.create({
   box: {
-    height: 100,
-    width: 100,
+    height: '5%',
+    width: "auto",
     backgroundColor: "red",
     position: "absolute",
-    top: 0,
-    left: 0,
+    top: 50,
+    left: 10,
     zIndex: 69,
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: "row"
   },
+  icon: {
+    height: 35,
+    width : 35,
+    position: "relative"
+  }
 });
