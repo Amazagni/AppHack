e5aa7068441cb50a327c24a7389e0eee6a46281a
Piotreqsl <piotreksl2002@go2.pl>
2023-04-02 03:14:44 +0200

    Few hardcoded acheivements

--- src/components/achievement/Achievement.js
+++ src/components/achievement/Achievement.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useRef, useCallback, useMemo } from "react";
 import { View, Image, Text, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Layout } from "react-native-reanimated";
 
-const Achievement = ({ imageSource, achievementTitle }) => {
+const Achievement = ({ imageSource, achievementTitle, style, ...props }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={imageSource} style={styles.image} />
-      </View>
-      <Text style={styles.title}>{achievementTitle}</Text>
+    <View style={{ ...styles.container, ...style }}>
+      <TouchableOpacity
+        onPress={props.onPress}
+        style={{ alignItems: "center" }}
+      >
+        <View style={styles.imageContainer}>
+          <Image
+            resizeMode="repeat"
+            source={imageSource}
+            style={styles.image}
+          />
+        </View>
+        <Text style={styles.title}>{achievementTitle}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -27,6 +38,8 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
     margin: 5,
+    flexGrow: 2,
+    maxWidth: "45%",
   },
   imageContainer: {
     width: 100,
@@ -34,14 +47,15 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     overflow: "hidden",
     marginBottom: 10,
+    padding: 10,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
   image: {
-    width: 80,
-    height: 80,
-    resizeMode: "contain",
+    width: 100,
+    height: 100,
+    resizeMode: "cover",
   },
   title: {
     fontSize: 18,
